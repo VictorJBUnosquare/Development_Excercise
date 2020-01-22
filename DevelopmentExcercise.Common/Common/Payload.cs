19e540ad54e92f03995ac65256387f79d0dced0c
Victor Jimenez <vjimenez@bofifederalbank.com>
2020-01-22 15:33:28 -0600

    Created project

---
+++ DevelopmentExcercise.Common/Common/Payload.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Common
+{
+	public class Payload<T>
+	{
+		public string Message { get; set; }
+		public T Data { get; set; }
+		public int Code { get; set; }
+
+		public Payload()
+		{
+			Message = "Succefull Operation";
+			Code = 200;
+		}
+	}
+}
