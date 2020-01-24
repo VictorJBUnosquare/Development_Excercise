dc9d8abe662ef34d2f058a88e3b7f2d664133586
Victor Jimenez <vjimenez@bofifederalbank.com>
2020-01-24 11:24:31 -0600

    Added angular project

---
+++ backend-Development_Excercise/DevelopmentExcercise.ApplicationContext/Configurations/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using DevelopmentExcercise.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevelopmentExcercise.ApplicationContext.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> entity)
+        {
+            entity.ToTable("Products");
+            entity.HasKey(p => p.Id);
+            entity.Property(p => p.Name).IsRequired().HasMaxLength(50);
+            entity.Property(p => p.Description).HasMaxLength(100);
+            entity.Property(p => p.Company).IsRequired().HasMaxLength(50);
+        }
+    }
+}
---
+++ backend-Development_Excercise/DevelopmentExcercise.Models/Domain/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevelopmentExcercise.Models.Domain
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? AgeRestriction { get; set; }
+        public string Company { get; set; }
+        public decimal Price { get; set; }
+    }
+}
