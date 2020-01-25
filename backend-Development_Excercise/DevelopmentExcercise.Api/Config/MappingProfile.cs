bc896f7c9bec62601f5ffea6f6bc7c893650378a
Victor Jimenez <vjimenez@bofifederalbank.com>
2020-01-24 18:02:33 -0600

    Added automapper

---
+++ backend-Development_Excercise/DevelopmentExcercise.Api/Config/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using DevelopmentExcercise.Api.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevelopmentExcercise.Api.Config
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //Domain to ViewModel
+            //CreateMap<Product, ProductViewModel>();
+
+            //ViewModel to Domain
+            //CreateMap<ProductViewModel, Product>();
+        }
+    }
+}
---
+++ backend-Development_Excercise/DevelopmentExcercise.Api/ViewModels/ProductViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevelopmentExcercise.Api.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required.")]
+        [MaxLength(50, ErrorMessage = "Length should not exceed {1} characters")]
+        public string Name { get; set; }
+
+        [MaxLength(100, ErrorMessage = "Length should not exceed {1} characters")]
+        public string Description { get; set; }
+        public int? AgeRestriction { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required.")]
+        [MaxLength(50, ErrorMessage = "Length should not exceed {1} characters")]
+        public string Company { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required.")]
+        public decimal? Price { get; set; }
+    }
+}
